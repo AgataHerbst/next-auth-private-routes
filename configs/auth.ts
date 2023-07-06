6b6e27f9e5a5346271284572f4373c42ac25748a
AgataHerbst <125795402+AgataHerbst@users.noreply.github.com>
2023-07-06 17:12:57 +0300

    new commit

--- configs/auth.ts
+++ configs/auth.ts
@@ -35,11 +35,3 @@ export const authOptions = {
   }
 };
 
-const resf = NextAuth(authOptions);
-
-export default (...params) => {
-  const [req] = params;
- // console.log('pages/api/auth/[...nextauth].js ');
-  //console.log('>> ', req.method, ' запрос на', req.url, req.query);
-  return resf(...params);
-};
